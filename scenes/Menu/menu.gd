bef1daa270da7260ee7af4de67f750def2af506d
Pawick69 <Patrick2730@gmail.com>
2023-10-10 17:18:48 +0200

    Background music
    and slider to change music volume

--- scenes/Menu/menu.gd
+++ scenes/Menu/menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var master_bus = AudioServer.get_bus_index("Master")
+
 func _on_start_knap_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
@@ -11,3 +13,12 @@ func _on_quit_knap_pressed():
 	get_tree().quit()
 
 
+
+
+func _on_music_sound_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
