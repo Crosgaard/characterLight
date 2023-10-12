30905d2a8d72f188ccca001c3bb9150e40e2b6f6
Crosgaard <conrad@therosgaards.dk>
2023-10-12 16:03:41 +0200

    Code layout

--- scenes/Menu/menu.gd
+++ scenes/Menu/menu.gd
@@ -6,7 +6,6 @@ func _on_start_knap_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 func _on_controls_knap_pressed():
-	pass
 	get_tree().change_scene_to_file("res://scenes/Menu/controls_menu.tscn")
 
 func _on_quit_knap_pressed():
